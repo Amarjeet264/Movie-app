f0f118165007785cc7c79a14681061aa5b682169
Amarjeet264 <yaamarjeet264@gmail.com>
2023-07-09 03:16:37 +0530

    Add existing project files to Git

---
+++ src/App.js
@@ -0,0 +1,13 @@
+
+import Main from './components/Main';
+
+function App() {
+  return (
+    <div>
+      <Main/>
+      {/* <h1 className='bg-red'>hello</h1> */}
+    </div>
+  );
+}
+
+export default App;
